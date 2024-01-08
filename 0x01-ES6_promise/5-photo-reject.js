e259e175bf20e37cdfa29fffdd3d54eed0101482
Muthamaki Creations <muthamakicreations@gmail.com>
2024-01-08 21:38:36 +0000

    MoreFiles

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,15 @@
-import uploadPhoto from './5-photo-reject';
+export default function uploadPhoto(fileName) {
+  return new Promise((resolve, reject) => {
+    // Simulating some processing logic
+    const isValid = Math.random() < 0.5;
 
-console.log(uploadPhoto('guillaume.jpg'));
+    if (isValid) {
+      // Simulating successful processing
+      resolve(`File ${fileName} has been processed successfully`);
+    } else {
+      // Simulating a failure with an error message
+      const errorMessage = `${fileName} cannot be processed`;
+      reject(new Error(errorMessage));
+    }
+  });
+}
