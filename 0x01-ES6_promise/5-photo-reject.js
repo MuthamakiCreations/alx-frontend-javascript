e76a53e427f8903398d4d8f687ce1041deb9ebe5
Muthamaki Creations <muthamakicreations@gmail.com>
2024-01-09 18:41:23 +0000

    Corrections

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,15 +1,3 @@
-export default function uploadPhoto(fileName) {
-  return new Promise((resolve, reject) => {
-    // Simulating some processing logic
-    const isValid = Math.random() < 0.5;
-
-    if (isValid) {
-      // Simulating successful processing
-      resolve(`File ${fileName} has been processed successfully`);
-    } else {
-      // Simulating a failure with an error message
-      const errorMessage = `${fileName} cannot be processed`;
-      reject(new Error(errorMessage));
-    }
-  });
+export default function uploadPhoto(filename) {
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
-
-  return Promise.allSettled([promise1, promise2])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
-    })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
